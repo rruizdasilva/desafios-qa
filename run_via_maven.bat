ac56c8fde517db7004a1df0ee829cc67a35e5fa8
Roger <roger.dasilva@ul.com>
2020-08-30 17:33:42 -0300

    Passando token como parâmetro.

--- run_via_maven.bat
+++ run_via_maven.bat
@@ -1,2 +1,2 @@
 cls
-mvn test -DtestSuiteFile=src/test/resources/suites/SuiteTest.xml -Dtokencode=1a5fc183-665e-404b-8dec-51e69aa02766
+mvn test -DtestSuiteFile=src/test/resources/suites/SuiteTest.xml -Dtokencode=%1
